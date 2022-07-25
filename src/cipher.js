72370ea2fc9e2918828ec3fdcacbd1e83cdefa16
Juliene-Araujo <julienefjs@gmail.com>
2022-07-25 11:25:11 -0300

    HTML, css e encode

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,25 @@
 const cipher = {
-  // ...
+  
+  encode: function(shiftMenssage, offset){
+  
+    let index
+    let cifrar
+    let letras = [65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90]
+    console.log(letras.length)
+    for(index = 0>=65; index <=90; index ++){
+      cifrar= shiftMenssage.charCodeAt(index)
+      console.log(cifrar= shiftMenssage.charCodeAt(index))
+    }
+    
+    return String.fromCharCode ((cifrar))
+    
+  }, 
+
+  //decode: fucntion (mensagem, offset){
+    
+    
+  //}
+
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 import cipher from './cipher.js';
 
+//let textUser = (document.getElementById("textuser").value)
+//let offset = (document.getElementById("offset").value)
+let bntcifrar = document.getElementById("bntcifrar")
+let bntdecifrar = document.getElementById("bntdecifrar")
+
+
+
+bntcifrar.addEventListener("click", function(e){
+    let mensagem = (document.getElementById("textuser").value)
+    let offset = (document.getElementById("offset").value)
+    alert(cipher.encode(mensagem, offset))
+})
+
+
+bntdecifrar.addEventListener("click", function(e){
+    let textUser= (document.getElementById("textuser").value)
+    let offset= (document.getElementById("offset").value)
+    alert(cipher.decode(mensagem, letras, offset))
+})
+
+
 console.log(cipher);
