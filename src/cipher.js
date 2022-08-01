286778b97da76916f95713def352cef568c58b05
Juliene-Araujo <julienefjs@gmail.com>
2022-08-01 11:54:16 -0300

    typeError

--- src/cipher.js
+++ src/cipher.js
@@ -1,23 +1,38 @@
 const cipher = {
 
-  encode: function (string, offset) {
+  encode: function (offset, mensagem) {
+
+    if(typeof mensagem!= "string" || typeof offset != "number" || mensagem==="" || offset===0){
+
+      throw new TypeError("Mensagem ou deslocamento inválido")
+
+    }
+
     let textocifrado = ""
 
-    for (let index = 0; index <= string.length - 1; index++) {
-      const cifrar = ((string.charCodeAt(index) - 65 + offset) % 26 + 65)
+    for (let index = 0; index <= mensagem.length - 1; index++) {
+      const cifrar = ((mensagem.charCodeAt(index) - 65 + offset) % 26 + 65)
       const letracifrada = String.fromCharCode(cifrar)
       textocifrado = textocifrado + letracifrada
     }
     return textocifrado
   },
 
-  decode: function (string, offset) {
-    let textodecifrado=""
 
-    for (let index = 0; index <= string.length - 1; index++) {
-      const decifrar = ((string.charCodeAt(index)- 90 - offset) % 26 + 90)
+  decode: function (offset, mensagem) {
+
+    if(typeof mensagem!= "string" || typeof offset != "number" || mensagem==="" || offset===0){
+
+      throw new TypeError("Mensagem ou deslocamento inválido")
+
+    }
+
+    let textodecifrado = ""
+
+    for (let index = 0; index <= mensagem.length - 1; index++) {
+      const decifrar = ((mensagem.charCodeAt(index) - 90 - offset) % 26 + 90)
       const letradecifrada = String.fromCharCode(decifrar)
-      textodecifrado = textodecifrado+letradecifrada 
+      textodecifrado = textodecifrado + letradecifrada
     }
     return textodecifrado
   }
--- src/index.js
+++ src/index.js
@@ -2,28 +2,45 @@ import cipher from './cipher.js';
 
 let bntcifrar = document.getElementById("buttoncifrar")
 let bntdecifrar = document.getElementById("buttondecifrar")
+const texto = document.getElementById("textuser")
+const deslocamento = document.getElementById("offset")
+
+
 
 
 
 bntcifrar.addEventListener("click", function (e) {
-    let string = (document.getElementById("textuser").value)
-    let offset = Number(document.getElementById("offset").value)
-    let resul = cipher.encode(string, offset)
-    alert(resul)
+    e.preventDefault()
 
+    const mensagem = texto.value.toUpperCase()
+    const offset= Number(deslocamento.value)
 
+    try {
+        const resul = cipher.encode(offset, mensagem)
+        alert(resul)
 
+    } catch (error) {
+       alert (error.message)
+    }
 })
+
+
 
 
 
 bntdecifrar.addEventListener("click", function (e) {
-    let string = (document.getElementById("textuser").value)
-    let offset = Number(document.getElementById("offset").value)
-    let resul2 = cipher.decode(string, offset)
-    alert(resul2)
+    e.preventDefault
+    
+    let mensagem = (texto.value.toUpperCase())
+    let offset = Number(deslocamento.value)
+
+    try {
+        let resul2 = cipher.decode(offset, mensagem)
+        alert(resul2)
+        
+    } catch (error) {
+        alert(error.message)
+        
+    }
 
 })
-
-
-console.log(cipher);
